e4c682952b84792078b21503a18f04f750f38fda
jeffkorby <82245490+jeffkorby@users.noreply.github.com>
2021-08-23 21:34:49 -0400

    Initial commit

---
+++ db/schema.sql
@@ -0,0 +1,31 @@
+DROP DATABASE IF EXISTS tracker_db;
+CREATE DATABASE tracker_db;
+
+USE tracker_db;
+
+DROP TABLE IF EXISTS department
+CREATE TABLE department (
+    id INT NOT NULL PRIMARY KEY,
+    department_name VARCHAR(30) NOT NULL, 
+)
+
+DROP TABLE IF EXISTS employee
+CREATE TABLE employee (
+    id INT PRIMARY KEY NOT NULL,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
+    role_id INT NOT NULL,
+    manager_id INT,
+    FOREIGN KEY (roles_id)
+    REFERENCES roles(id)
+)
+
+DROP TABLE IF EXISTS roles
+CREATE TABLE roles (
+    id INT PRIMARY KEY,
+    title VARCHAR(30) NOT NULL,
+    salary DECIMAL NOT NULL,
+    department_id INT NOT NULL,
+    FOREIGN KEY (department_id)
+    REFERENCES department(id)
+)
---
+++ index.js
@@ -0,0 +1,10 @@
+const mysql = require('mysql2');
+const inquirer = require('inquirer');
+const console = require('console.table');
+const express = require('express');
+
+const PORT = process.env.port || 4200;
+const app = express();
+
+app.listen(PORT, () =>
+    console.log(`Listening at port ${PORT}`));
